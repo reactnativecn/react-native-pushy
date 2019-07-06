b55a40c92ca181b3fda76ba1a35b555102a53e59
sunnylqm <sunnylqm@qq.com>
2019-07-06 21:52:34 +0800

    添加ts声明

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+export const downloadRootDir: string;
+export const packageVersion: string;
+export const currentVersion: string;
+export const isFirstTime: boolean;
+export const isRolledBack: boolean;
+
+export interface ExpiredResult {
+  expired: true;
+  downloadUrl: string;
+}
+
+export interface UpTodateResult {
+  upToDate: true;
+}
+
+export interface UpdateAvailableResult {
+  update: true;
+  name: string; // version name
+  hash: string;
+  description: string;
+  metaInfo: string;
+  updateUrl: string;
+  pdiffUrl: string;
+  diffUrl: string;
+}
+
+export function checkUpdate(appkey: string): Promise<ExpiredResult | UpTodateResult | UpdateAvailableResult>;
+
+export function downloadUpdate(options: UpdateAvailableResult): Promise<undefined | string>;
+
+export function switchVersion(hash: string): void;
+
+export function switchVersionLater(hash: string): void;
+
+export function markSuccess(): void;
