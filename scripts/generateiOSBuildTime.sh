f9ff69015714af669b3839c18a34adfef321e3c6
sunnylqm <sunnylqm@qq.com>
2019-10-06 22:04:00 +0800

    Fix build time string type

--- scripts/generateiOSBuildTime.sh
+++ scripts/generateiOSBuildTime.sh
@@ -4,6 +4,6 @@ set -x
 # DEST=$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH
 # date +%s > "$DEST/pushy_build_time.txt"
 OLD_TEXT="NSString \*pushy_build_time"
-NEW_TEXT="    NSString \*pushy_build_time = $(date +%s)"
+NEW_TEXT="    NSString \*pushy_build_time = @\"$(date +%s)\";"
 TARGET_FILE="../../node_modules/react-native-update/ios/RCTHotUpdate/RCTHotUpdate.m"
 sed -i '' -e "s/.*$OLD_TEXT.*/$NEW_TEXT/" "$TARGET_FILE"
