76e5c6c59b3af79d6f854693a64e84d6032b28d6
steven <steven@wooza.com>
2023-03-16 14:45:36 +0800

    feat: project init

--- Example/testHotUpdate/detox.config.js
+++ Example/testHotUpdate/detox.config.js
@@ -44,7 +44,7 @@ module.exports = {
       headless: Boolean(process.env.CI),
       gpuMode: process.env.CI ? 'off' : undefined,
       device: {
-        avdName: 'Pixel_3a_API_33_arm64-v8a',
+        avdName: 'TestingAVD',
       },
       utilBinaryPaths: ['./cache/test-butler-app.apk'],
     },
@@ -58,7 +58,7 @@ module.exports = {
     'genymotion.emulator.name': {
       type: 'android.genycloud',
       device: {
-        recipeName: 'Pixel_3a_API_33_arm64-v8a',
+        recipeName: 'TestingAVD',
       },
       utilBinaryPaths: ['./cache/test-butler-app.apk'],
     },
