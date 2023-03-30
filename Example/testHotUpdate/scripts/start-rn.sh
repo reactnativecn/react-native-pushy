6fcfa0b50574712d6504c6d7061a15d2fb59fca2
steven <steven@wooza.com>
2023-03-30 12:50:33 +0800

    feat: fix

--- Example/testHotUpdate/scripts/start-rn.sh
+++ Example/testHotUpdate/scripts/start-rn.sh
@@ -4,7 +4,7 @@ trap 'kill $RN_PID' EXIT
 
 PLATFORM=$1
 
-npm start &
+kill -9 $(lsof -i :8081 | awk '{print $2}' | tail -n +2) & npm start &
 RN_PID=$!
 sleep 2 && curl>/dev/null http://localhost:8081/index.bundle
 wait $RN_PID
