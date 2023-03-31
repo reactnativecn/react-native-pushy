c57802e2d2c3df84c3fa50dc7ea47568f5bc27fd
steven <steven@wooza.com>
2023-03-31 21:35:25 +0800

    feat: fix

--- Example/testHotUpdate/detox.config.js
+++ Example/testHotUpdate/detox.config.js
@@ -1,100 +1,78 @@
 /** @type {Detox.DetoxConfig} */
 module.exports = {
-  logger: {
-    level: process.env.CI ? 'debug' : undefined,
-  },
   testRunner: {
     args: {
+      $0: 'jest',
       config: 'e2e/jest.config.js',
-      maxWorkers: process.env.CI ? 2 : undefined,
-      _: ['e2e'],
     },
-  },
-  artifacts: {
-    plugins: {
-      log: process.env.CI ? 'failing' : undefined,
-      screenshot: process.env.CI ? 'failing' : undefined,
+    jest: {
+      setupTimeout: 120000,
     },
   },
   apps: {
+    'ios.debug': {
+      type: 'ios.app',
+      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/testHotUpdate.app',
+      build:
+        'xcodebuild -workspace ios/testHotUpdate.xcworkspace -scheme testHotUpdate -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
+    },
     'ios.release': {
       type: 'ios.app',
       binaryPath:
         'ios/build/Build/Products/Release-iphonesimulator/testHotUpdate.app',
       build:
-        'export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -workspace ios/testHotUpdate.xcworkspace -UseNewBuildSystem=NO -scheme testHotUpdate -configuration Release -sdk iphonesimulator -derivedDataPath ios/build -quiet',
+        'xcodebuild -workspace ios/testHotUpdate.xcworkspace -scheme testHotUpdate -configuration Release -sdk iphonesimulator -derivedDataPath ios/build',
     },
-    "ios.debug": {
-      "type": "ios.app",
-      "binaryPath": "ios/build/Build/Products/Debug-iphonesimulator/testHotUpdate.app",
-      "build": "xcodebuild -workspace ios/testHotUpdate.xcworkspace -UseNewBuildSystem=NO -scheme testHotUpdate -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build",
-      "start": "scripts/start-rn.sh ios",
-    },
-    "android.debug": {
-      "type": "android.apk",
-      "binaryPath": "android/app/build/outputs/apk/debug/app-debug.apk",
-      "build": "cd android ; ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug ; cd -",
-      "start": "scripts/start-rn.sh android",
+    'android.debug': {
+      type: 'android.apk',
+      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+      build:
+        'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
+      reversePorts: [8081],
     },
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
       build:
-        'cd android ; ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release ; cd -',
+        'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release',
     },
   },
   devices: {
     simulator: {
       type: 'ios.simulator',
-      headless: Boolean(process.env.CI),
       device: {
         type: 'iPhone 14',
       },
     },
+    attached: {
+      type: 'android.attached',
+      device: {
+        adbName: '.*',
+      },
+    },
     emulator: {
       type: 'android.emulator',
-      headless: Boolean(process.env.CI),
-      gpuMode: process.env.CI ? 'off' : undefined,
       device: {
-        avdName: 'TestingAVD',
+        avdName: 'Pixel_3a_API_33_arm64-v8a',
       },
-      utilBinaryPaths: ['./cache/test-butler-app.apk'],
-    },
-    'genymotion.emulator.uuid': {
-      type: 'android.genycloud',
-      device: {
-        recipeUUID: 'a50a71d6-da90-4c67-bdfa-5b602b0bbd15',
-      },
-      utilBinaryPaths: ['./cache/test-butler-app.apk'],
-    },
-    'genymotion.emulator.name': {
-      type: 'android.genycloud',
-      device: {
-        recipeName: 'TestingAVD',
-      },
-      utilBinaryPaths: ['./cache/test-butler-app.apk'],
     },
   },
   configurations: {
-    'ios.sim.release': {
-      device: 'simulator',
-      app: 'ios.release',
-    },
     'ios.sim.debug': {
       device: 'simulator',
       app: 'ios.debug',
     },
-    'ios.manual': {
-      type: 'ios.manual',
-      behavior: {
-        launchApp: 'manual',
-      },
-      artifacts: false,
-      session: {
-        autoStart: true,
-        server: 'ws://localhost:8099',
-        sessionId: 'com.wix.demo.react.native',
-      },
+    'ios.sim.release': {
+      device: 'simulator',
+      app: 'ios.release',
+    },
+    'android.att.debug': {
+      device: 'attached',
+      app: 'android.debug',
+    },
+    'android.att.release': {
+      device: 'attached',
+      app: 'android.release',
     },
     'android.emu.debug': {
       device: 'emulator',
