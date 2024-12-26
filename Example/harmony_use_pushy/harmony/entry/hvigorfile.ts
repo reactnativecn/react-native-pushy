15ced8f977822847ca4299eb8dc30f031a50293c
HeYanbo <bozaigao.a@gmail.com>
2024-12-26 10:50:55 +0800

    add pushy build time logic

--- Example/harmony_use_pushy/harmony/entry/hvigorfile.ts
+++ Example/harmony_use_pushy/harmony/entry/hvigorfile.ts
@@ -1,6 +1,32 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import fs from 'fs';
+import path from 'path';
+export function generatePushyBuildTime(str?: string) {
+    return {
+        pluginId: 'PushyBuildTimePlugin',
+        apply(pluginContext) {
+            pluginContext.registerTask({
+                name: 'pushy_build_time',
+                run: (taskContext) => {
+                    const metaFilePath = path.resolve(__dirname, 'src/main/resources/rawfile/meta.json');
+                    const dirPath = path.dirname(metaFilePath);
+                    if (!fs.existsSync(dirPath)) {
+                        fs.mkdirSync(dirPath, { recursive: true });
+                    }
+                    const buildTime = new Date().toISOString();
+                    const metaContent = { pushy_build_time : buildTime };
+                    fs.writeFileSync(metaFilePath, JSON.stringify(metaContent, null, 4));
+                    console.log(`Build time written to ${metaFilePath}`);
+
+                },
+                dependencies: [],
+                postDependencies: ['default@BuildJS']
+            })
+        }
+    }
+}
 
 export default {
     system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+    plugins:[generatePushyBuildTime()]         /* Custom plugin to extend the functionality of Hvigor. */
 }
